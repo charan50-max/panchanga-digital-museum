fc2a80758cf42abf0848e53d777957fc5bc9f26c
Charan Ramesh Shetty <shettycharan014@gmail.com>
2026-09-25 01:57:07 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "https://panchanga-digital-museum.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
